a5d97f7503cb59d473a701944ce128db960aacbb
enrolo <andrade.rolando90@gmail.com>
2018-12-16 03:39:34 -0300

    se agregó ocultamiento de botones y textview no seleccionados para el conteo; en vias de crear metodos para cada botón

--- app/src/main/java/com/example/rolo/conteo_vehicular/Conteo.java
+++ app/src/main/java/com/example/rolo/conteo_vehicular/Conteo.java
@@ -3,18 +3,28 @@ package com.example.rolo.conteo_vehicular;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Conteo extends AppCompatActivity {
-    ImageButton bbici,bmoto,bcolectivo,btaxi,bauto,bcamion,bmicro,bbus,bcamiongrande;
+    ImageButton bbici,bmoto,bcolectivo,btaxi,bauto,bcamion,bmicro,bbus,bcamiongrande,barriba,bizquierda,bderecha;
     TextView tvbici,tvmoto,tvcolectivo,tvtaxi,tvauto,tvcamion,tvmicro,tvbus,tvcamiongrande;
+    boolean[] movs;
+    boolean[] vehiculos;
+    int vehiculo=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conteo);
+
+        //datos enviados desde activity vehiculos
+        movs = getIntent().getBooleanArrayExtra("movs");
+        vehiculos = getIntent().getBooleanArrayExtra("vehiculos");
+
 
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -30,6 +40,9 @@ public class Conteo extends AppCompatActivity {
         bmicro = (ImageButton) findViewById(R.id.bmicro);
         bbus = (ImageButton) findViewById(R.id.bbus);
         bcamiongrande = (ImageButton) findViewById(R.id.bcamiongrande);
+        barriba = (ImageButton) findViewById(R.id.barriba);
+        bizquierda= (ImageButton) findViewById(R.id.bizquierda);
+        bderecha= (ImageButton) findViewById(R.id.bderecha);
 
         tvbici = (TextView) findViewById(R.id.tvbici);
         tvmoto = (TextView) findViewById(R.id.tvmoto);
@@ -40,5 +53,50 @@ public class Conteo extends AppCompatActivity {
         tvmicro = (TextView) findViewById(R.id.tvmicro);
         tvbus = (TextView) findViewById(R.id.tvbus);
         tvcamiongrande = (TextView) findViewById(R.id.tvcamiongrande);
+
+        //ocultando los botones no seleccionados
+        if(vehiculos[0]==false) {
+            bbici.setVisibility(View.INVISIBLE);
+            tvbici.setVisibility(View.INVISIBLE);
+        }if(vehiculos[1]==false) {
+            bmoto.setVisibility(View.INVISIBLE);
+            tvmoto.setVisibility(View.INVISIBLE);
+        }if(vehiculos[2]==false) {
+            bcolectivo.setVisibility(View.INVISIBLE);
+            tvcolectivo.setVisibility(View.INVISIBLE);
+        }if(vehiculos[3]==false) {
+            btaxi.setVisibility(View.INVISIBLE);
+            tvtaxi.setVisibility(View.INVISIBLE);
+        }if(vehiculos[4]==false) {
+            bauto.setVisibility(View.INVISIBLE);
+            tvauto.setVisibility(View.INVISIBLE);
+        }if(vehiculos[5]==false) {
+            bcamion.setVisibility(View.INVISIBLE);
+            tvcamion.setVisibility(View.INVISIBLE);
+        }if(vehiculos[6]==false) {
+            bmicro.setVisibility(View.INVISIBLE);
+            tvmicro.setVisibility(View.INVISIBLE);
+        }if(vehiculos[7]==false) {
+            bbus.setVisibility(View.INVISIBLE);
+            tvbus.setVisibility(View.INVISIBLE);
+        }if(vehiculos[8]==false) {
+            bcamiongrande.setVisibility(View.INVISIBLE);
+            tvcamiongrande.setVisibility(View.INVISIBLE);
+        }
+        //ocultando botones de movimiento no seleccionados
+        if(movs[0]==false){
+            barriba.setVisibility(View.INVISIBLE);
+        }
+        if(movs[1]==false){
+            bizquierda.setVisibility(View.INVISIBLE);
+        }
+        if(movs[2]==false){
+            bderecha.setVisibility(View.INVISIBLE);
+        }
+
+
+    }
+    public void bici(View view){
+        vehiculo = 1;
     }
 }
--- app/src/main/java/com/example/rolo/conteo_vehicular/Vehiculo.java
+++ app/src/main/java/com/example/rolo/conteo_vehicular/Vehiculo.java
@@ -3,6 +3,7 @@ package com.example.rolo.conteo_vehicular;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -37,21 +38,23 @@ public class Vehiculo extends AppCompatActivity {
         boolean[] vehiculos ={chkbici.isChecked(),chkmoto.isChecked(),chkcolectivo.isChecked(),chktaxi.isChecked(),chkauto.isChecked(),chkcamion.isChecked(),chkmicro.isChecked(),chkbus.isChecked(),chkcamiongrande.isChecked()};
         boolean v=false;
         //evaluando existencia de al menos una elección de vehículo
-        for (int i=0; i<=vehiculos.length;i++){
+        for (int i=0;i<=8;i++){
             if(vehiculos[i]){
                 v=true;
                 break;
             }
+            String b = String.valueOf(vehiculos[i]);
+            Log.i("Vehiculo",b);
         }
 
-        if(v){
+        if(v==false){
+            Toast.makeText(this,"Debe seleccionar al menos un vehículo",Toast.LENGTH_SHORT).show();
+        }
+        else{
             Intent abrirconteo = new Intent(Vehiculo.this, Conteo.class);
             abrirconteo.putExtra("movs",movs);
             abrirconteo.putExtra("vehiculos", vehiculos);
             startActivity(abrirconteo);
-        }
-        else{ 
-            Toast.makeText(this,"Debe seleccionar al menos un vehículo",Toast.LENGTH_SHORT).show();
         }
     }
 }
